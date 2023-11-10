ec2f4781c79daa5aa6436bcedf94890a82dc0432
Ming Fong <mingfong@berkeley.edu>
2023-11-09 20:05:52 -0800

    Update train_fastsim.sh script

--- train_fastsim.sh
+++ train_fastsim.sh
@@ -18,4 +18,4 @@ conda activate jax
 
 #run the application:
 #applications may perform better with --gpu-bind=none instead of --gpu-bind=single:1 
-srun -n 1 -c 128 --cpu_bind=cores -G 1 --gpu-bind=single:1  python3 MLP.py
+srun -n 1 -c 128 --cpu_bind=cores -G 1 --gpu-bind=single:1 python3 MLP.py
